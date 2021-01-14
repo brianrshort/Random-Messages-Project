7ea5aa492b67331d40f46d483913f47fc08caced
Brian Short <brian.ralph.short@gmail.com>
2021-01-14 10:18:38 -0500

    Add randomized greeting function to app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //A simple, silly fortune-telling app.
+
+//A function to select a random greeting
+function greeting() {
+    const arr = ["Welcome, traveller!", "Greetings, querant!", "Sup chump."];
+    const randNum = Math.floor(Math.random() * arr.length); 
+    return arr[randNum];
+}
+
