9756fe428ac2a3aba22f2dcf53361dca9768bf0f
Brian Short <brian.ralph.short@gmail.com>
2021-01-14 10:19:12 -0500

    Add randomized body (fortune) function to app.js

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function greeting() {
     return arr[randNum];
 }
 
+//A function to select a random fortune
+function tellFortune() {
+    const arr = ["Beware the ides of January!", "Behold, a rider on a pale horse, and its name is inconvenient snowfall!", "A new life is coming soon."];
+    const randNum = Math.floor(Math.random() * arr.length);
+    return arr[randNum];
+}
+
